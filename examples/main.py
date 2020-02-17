1cb09c93f21011b4a648e9c07b08f6a47f87b636
Kyle Kirkby <kyle.kirkby@linaro.org>
2020-02-17 16:05:18 +0000

    Updated the youtube_upload example script

--- examples/main.py
+++ examples/main.py
@@ -10,9 +10,23 @@ import os
 
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..')))
+try:
+    from secrets import SECRETS_FILE, SECRETS_DIR
+except Exception as e:
+    SECRETS_FILE = ""
+    SECRETS_DIR = ""
 
 from connect_youtube_uploader import ConnectYoutubeUploader
 
-video_manager = ConnectYoutubeUploader(
-    "")
-updated = video_manager.get_video_id_based_on_session_id("yvr18-100k")
+video_manager = ConnectYoutubeUploader(SECRETS_DIR, SECRETS_FILE)
+path = video_manager.download_video("https://static.linaro.org/connect/bud20/videos/san19-405.mp4",
+                             "/home/kyle/Documents/scripts_and_snippets/ConnectAutomation/connect_youtube_uploader/output/")
+video_options = {
+    "file": path,
+    "title": "BUD20-212: Test video",
+    "description": "Test Video",
+    "keywords": "bud20,Open Source,Arm, budapest",
+                "category": "28",
+                "privacyStatus": "private"
+}
+uploaded = video_manager.upload_video(video_options)
