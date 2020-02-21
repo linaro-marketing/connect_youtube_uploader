ce5c57a7025b240f80cf07e696672d7d1eab8047
Kyle Kirkby <kyle.kirkby@linaro.org>
2020-02-21 11:06:20 +0000

    Updated the example to include the the setting of the custom generated social media share thumbnail

--- examples/main.py
+++ examples/main.py
@@ -16,17 +16,22 @@ except Exception as e:
     SECRETS_FILE = ""
     SECRETS_DIR = ""
 
+from sched_data_interface import SchedDataInterface
 from connect_youtube_uploader import ConnectYoutubeUploader
 
 video_manager = ConnectYoutubeUploader(SECRETS_DIR, SECRETS_FILE)
-path = video_manager.download_video("https://static.linaro.org/connect/bud20/videos/san19-405.mp4",
-                             "/home/kyle/Documents/scripts_and_snippets/ConnectAutomation/connect_youtube_uploader/output/")
-video_options = {
-    "file": path,
-    "title": "BUD20-212: Test video",
-    "description": "Test Video",
-    "keywords": "bud20,Open Source,Arm, budapest",
-                "category": "28",
-                "privacyStatus": "private"
+
+# path_to_downloaded_video = video_manager.download_video("https://static.linaro.org/connect/bud20/videos/bud20-215.mp4",
+#                                 "/home/kyle/Documents/scripts_and_snippets/ConnectAutomation/connect_youtube_uploader/output/")
+video_options={
+    "file": "/home/kyle/Documents/scripts_and_snippets/ConnectAutomation/connect_youtube_uploader/output/bud20-215.mp4",
+            "title": "BUD20-215: Test video",
+            "description": "Test Video",
+            "keywords": "bud20,Open Source,Arm, budapest",
+            "category": "28",
+            "privacyStatus": "private"
 }
-uploaded = video_manager.upload_video(video_options)
+video_id = video_manager.upload_video(video_options)
+print(video_id)
+thumbnail_set = video_manager.set_custom_thumbnail("/home/kyle/Documents/scripts_and_snippets/ConnectAutomation/social_image_generator/output/BUD20-215.png", video_id)
+print(thumbnail_set)
